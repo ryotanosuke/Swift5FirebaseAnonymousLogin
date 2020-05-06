09e5f263814075b939c8cd3333a7919d9c3ecdf1
ryotanosuke <sub_player-github@yahoo.co.jp>
2020-05-06 16:27:28 +0900

    Initial Commit

---
+++ Swift5FirebaseAnonymousLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5FirebaseAnonymousLogin
+//
+//  Created by ryota on 2020/05/06.
+//  Copyright © 2020 Honda Ryota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
